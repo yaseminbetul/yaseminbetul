7141ef7621f5765e569dfa2a8e17194c7a35e4d6
yaseminbetul <126774410+yaseminbetul@users.noreply.github.com>
2023-06-11 10:11:48 +0300

    Add files via upload

---
+++ Odev.java1234.java
@@ -0,0 +1,42 @@
+package Hafta10;
+
+public class Odev {
+    /*
+    Odev - 1 : ASCII tablodaki 0 - 255 arasındaki degerlerin karşılıklarını yazan kod blogunu yazınız.
+       
+        for deger in range(256):
+         karakter = chr(deger)
+        print(f"{deger}: {karakter}")
+
+   
+   
+    Odev - 2 : girilen cumleyi tersten yazdiran kodu yaziniz.
+         
+          cumle = input("Bir cumle girin: ")
+          tersten_cumle = cumle[::-1]
+          print("Cumlenin ters hali:", tersten_cumle)
+
+
+
+    Odev - 3 : Girilen ifadenin Binary(0-1) olup olmadıgnı kontrol eden kodu yaziniz.
+             
+              binary kontrol(ifade):
+              for karakter in ifade:
+                
+                if karakter != '0' and karakter != '1':
+                  return False
+                return True
+
+           girilen_ifade = input("Bir ifade girin: ")
+           if binary kontrol(girilen_ifade):
+           
+            print("Girilen ifade bir binary ifadedir.")
+     else:
+           print("Girilen ifade bir binary ifade degildir.")
+
+
+
+ */
+
+
+}
